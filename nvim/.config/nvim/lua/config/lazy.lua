7d3990f711e5bf25e311ee747d9855b39d2f2365
Boklau <bokgaming9@gmail.com>
2025-05-22 14:35:32 -0500

    changed color of line numbers

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -59,3 +59,10 @@ require("lazy").setup({
     },
   },
 })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "red", bg = "black" })
+function LineNumberColors()
+  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "red", bold = false })
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffe5b4", bold = true })
+  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "red", bold = false })
+end
+LineNumberColors()
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.api.nvim_set_hl(0, "LineNr", { fg = "red", bg = "black" })
+function LineNumberColors()
+  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "red", bold = false })
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffe5b4", bold = true })
+  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "red", bold = false })
+end
+LineNumberColors()
